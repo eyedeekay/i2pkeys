bfcde005b3cde642477f223561c2e85ffc6c46cf
Haris Khan <hk@hariskhan.dev>
2024-10-26 19:18:08 -0400

    Refactored LoadKeysIncompat -Previously: would log errors regardless at the end -Better error handling

--- I2PAddr.go
+++ I2PAddr.go
@@ -62,18 +62,25 @@ func fileExists(filename string) (bool, error) {
 	return !info.IsDir(), nil
 }
 
-// load keys from non standard format
-func LoadKeysIncompat(r io.Reader) (k I2PKeys, err error) {
+func LoadKeysIncompat(r io.Reader) (I2PKeys, error) {
 	log.Debug("Loading keys from reader")
 	var buff bytes.Buffer
-	_, err = io.Copy(&buff, r)
-	if err == nil {
-		parts := strings.Split(buff.String(), "\n")
-		k = I2PKeys{I2PAddr(parts[0]), parts[1]}
-		log.WithField("keys", k).Debug("Loaded keys")
+	_, err := io.Copy(&buff, r)
+	if err != nil {
+		log.WithError(err).Error("Error copying from reader, did not load keys")
+		return I2PKeys{}, fmt.Errorf("error copying from reader: %w", err)
 	}
-	log.WithError(err).Error("Error copying from reader, did not load keys")
-	return
+
+	parts := strings.Split(buff.String(), "\n")
+	if len(parts) < 2 {
+		err := errors.New("invalid key format: not enough data")
+		log.WithError(err).Error("Error parsing keys")
+		return I2PKeys{}, err
+	}
+
+	k := I2PKeys{I2PAddr(parts[0]), parts[1]}
+	log.WithField("keys", k).Debug("Loaded keys")
+	return k, nil
 }
 
 // load keys from non-standard format by specifying a text file.
